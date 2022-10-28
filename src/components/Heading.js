8c619082d188b8b7c6b3a9445f867e4357a72fd9
Hanna Huzava <guzovaann@gmail.com>
2022-10-28 19:26:28 +0200

    feat: add component Heading

---
+++ src/components/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <img
+        class="App-logo"
+        src="https://img.freepik.com/vector-premium/ilustracion-simple-logotipo-hotel_434503-736.jpg"
+        alt="logo"
+      />
+    </header>
+  );
+};
+
+export default Heading;
