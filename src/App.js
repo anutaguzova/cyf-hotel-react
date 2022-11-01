425d49347b73f0060873a57ad10b688e206281ba
Hanna Huzava <guzovaann@gmail.com>
2022-11-01 21:35:31 +0100

    feat: add Restaurant component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Heading from "./components/Heading";
 import TouristInfoCards from "./components/TouristInfoCards";
 import Bookings from "./components/Bookings";
+import Restaurant from "./components/Restaurant";
 import Footer from "./components/Footer";
 import "./App.css";
 
@@ -11,6 +12,7 @@ const App = () => {
       <Heading />
       <TouristInfoCards />
       <Bookings />
+      <Restaurant />
       <Footer />
     </div>
   );
